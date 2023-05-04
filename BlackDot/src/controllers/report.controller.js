ee10a6b0153ca3f5b88412b82f3d1f7a199974d6
salgue441 <carlossalguero441@gmail.com>
2023-05-04 15:12:52 -0600

    Hot fix

--- BlackDot/src/controllers/report.controller.js
+++ BlackDot/src/controllers/report.controller.js
@@ -1,17 +1,21 @@
 const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
-const chromium = require("chrome-aws-lambda");
+const Xvfb = require("xvfb");
+const xvfb = new Xvfb();
 
 const generateTemplate = async (req, res) => {
   try {
     const { graphImage, reportTitle } = req.body;
+
+    // Read the logo image file and encode it as base64
     const logoImage = fs.readFileSync(
       path.join(__dirname, "../../public/assets/$zebrands-brand-logo.svg")
-    )
+    );
+
     const logoImageEncoded = logoImage.toString("base64");
 
-    // Generate PDF
+    // Generate the PDF
     const template = await ejs.renderFile(
       path.join(__dirname, "../views/static/reports/template.ejs"),
       {
@@ -23,30 +27,31 @@ const generateTemplate = async (req, res) => {
       }
     );
 
-    const browser = await chromium.puppeteer.launch({
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      age: chromium.args,
-    })
+    // Start the virtual display
+    xvfb.startSync();
+
+    // Generate the PDF
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+    });
 
     const page = await browser.newPage();
     await page.setContent(template, { waitUntil: "networkidle0" });
-
     await page.pdf({
       path: "public/reports/report.pdf",
       format: "A4",
       printBackground: true,
-    })
+    });
 
+    // Close the browser and stop the virtual display
     await browser.close();
+    xvfb.stopSync();
+
     const fileName = "report";
     const domain = process.env.DOMAIN || "http://localhost:3000";
 
-    return res.status(200).json({
-      success: true,
-      fileName,
-      domain
-    })
+    return res.status(201).json({ success: true, fileName, domain });
   }
   catch (error) {
     console.log(error);
