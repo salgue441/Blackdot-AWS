407120ad7ee37a45b3d8113edb1ea17e1bc0ac2f
salgue441 <carlossalguero441@gmail.com>
2023-05-04 15:17:19 -0600

    Hot fix

--- BlackDot/src/controllers/report.controller.js
+++ BlackDot/src/controllers/report.controller.js
@@ -1,24 +1,18 @@
 const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
-const Xvfb = require("xvfb");
-const xvfb = new Xvfb({
-  silent: true,
-  xvfb_args: ["-screen", "0", "1280x800x24", "-ac"],
-});
+const puppeteer = require("chrome-aws-lambda");
 
 const generateTemplate = async (req, res) => {
   try {
     const { graphImage, reportTitle } = req.body;
 
-    // Read the logo image file and encode it as base64
     const logoImage = fs.readFileSync(
       path.join(__dirname, "../../public/assets/$zebrands-brand-logo.svg")
     );
 
     const logoImageEncoded = logoImage.toString("base64");
 
-    // Generate the PDF
     const template = await ejs.renderFile(
       path.join(__dirname, "../views/static/reports/template.ejs"),
       {
@@ -30,13 +24,11 @@ const generateTemplate = async (req, res) => {
       }
     );
 
-    // Start the virtual display
-    xvfb.startSync();
-
-    // Generate the PDF
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+      args: puppeteer.defaultArgs(),
+      defaultViewport: puppeteer.defaultViewport,
+      executablePath: await puppeteer.executablePath(),
+      headless: puppeteer.headless,
     });
 
     const page = await browser.newPage();
@@ -47,24 +39,21 @@ const generateTemplate = async (req, res) => {
       printBackground: true,
     });
 
-    // Close the browser and stop the virtual display
     await browser.close();
-    xvfb.stopSync();
 
     const fileName = "report";
     const domain = process.env.DOMAIN || "http://localhost:3000";
 
     return res.status(201).json({ success: true, fileName, domain });
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       error: error.message,
-    })
+    });
   }
-}
+};
 
 module.exports = {
-  generateTemplate
+  generateTemplate,
 }
