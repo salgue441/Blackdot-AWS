0dc96d31736d07defdcb434fd307821ecd9c78a6
salgue441 <carlossalguero441@gmail.com>
2023-05-04 16:21:50 -0600

    Hot fix

--- BlackDot/src/controllers/report.controller.js
+++ BlackDot/src/controllers/report.controller.js
@@ -1,61 +1,65 @@
+/**
+ * @file report.controller.js
+ * @brief Controller for report page
+ * @author Yuna Chung
+ * @version 1.0
+ * @date 2023.04.27
+ */
+
 const ejs = require("ejs");
 const fs = require("fs");
+const pdf = require("html-pdf");
 const path = require("path");
-const puppeteer = require("puppeteer");
 
 const generateTemplate = async (req, res) => {
-  try {
-    const { graphImage, reportTitle } = req.body;
-    const logoImage = fs.readFileSync(
-      path.join(__dirname, "../../public/assets/$zebrands-brand-logo.svg")
-    );
+  const { graphImage, reportTitle } = req.body;
 
-    const logoImageEncoded = logoImage.toString("base64");
+  // Read the logo image file and encode it as base64
+  const logoImage = fs.readFileSync(
+    path.join(__dirname, "../../public/assets/$zebrands-brand-logo.svg")
+  );
 
-    const template = await ejs.renderFile(
-      path.join(__dirname, "../views/static/reports/template.ejs"),
-      {
-        title: reportTitle,
-        graphImage: graphImage,
-        logoImage: `data:image/svg+xml;base64,${logoImageEncoded}`,
-        pageNumber: 1,
-        totalPages: 1,
-      }
-    );
+  const logoImageEncoded = logoImage.toString("base64");
 
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
-      headless: 'new',
-    });
+  // Generate the PDF
+  const template = await ejs.renderFile(
+    path.join(__dirname, "../views/static/reports/template.ejs"),
+    {
+      title: reportTitle,
+      graphImage: graphImage,
+      logoImage: `data:image/svg+xml;base64,${logoImageEncoded}`,
+      pageNumber: 1,
+      totalPages: 1,
+    }
+  );
 
-    const page = await browser.newPage();
-    await page.setContent(template);
-    await page.emulateMediaType("screen");
+  const options = {
+    height: "11in",
+    width: "8.5in",
+    header: {
+      height: "5mm",
+    },
+    footer: {
+      height: "5mm",
+    },
+  };
 
-    const pdf = await page.pdf({
-      format: "A4",
-      printBackground: true,
-      margin: {
-        top: "20px",
-        bottom: "40px",
-        left: "20px",
-        right: "20px",
-      },
-    });
+  const fileName = "report";
 
-    await browser.close();
+  const domain = process.env.DOMAIN || "http://localhost:3000";
 
-    res.contentType("application/pdf");
-    res.send(pdf);
+  pdf.create(template, options).toFile(`public/reports/${fileName}.pdf`, (error, data) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error generating the PDF",
+      });
+    }
 
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
+    return res.status(201).json({ success: true, fileName, domain })
+  });
 };
+
 
 module.exports = {
   generateTemplate,
