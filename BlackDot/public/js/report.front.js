bdd65e56204326561ed947d0c8346c8b211ef882
salgue441 <carlossalguero441@gmail.com>
2023-05-04 21:04:49 -0600

    Rewored PDF

--- BlackDot/public/js/report.front.js
+++ BlackDot/public/js/report.front.js
@@ -7,107 +7,57 @@
  *
  * @copyright Copyright 2023 (c) - MIT License
  */
+
 /**
  * @brief
- * Generates the report when the button is clicked.
- * Sends the data to the backend.
- * @param {string} canvasID - The ID of the canvas element
+ * Fetches the template from the server
+ * @param {Object} data - Data to be sent to the server
+ * @returns {String} - HTML template
  */
-const generateReport = async (canvasID, pageTitle) => {
-  // Clone and update the chart
-  const updateChart = clonedAndUpdateChart(canvasID);
-  const graphImage = updateChart.toBase64Image();
-
-  // Send the data to the backend
-  const data = {
-    graphImage: graphImage,
-    reportTitle: pageTitle,
-  };
-
+const fetchTemplate = async (data) => {
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  };
-
-  try {
-    const response = await fetch("/report/admin/generate", options);
-    const result = await response.json();
-
-    console.log(result);
-
-    window.open(`${result.domain}/reports/${result.fileName}.pdf`, "_blank");
-
-    const data = await generateCSV(canvasID);
-    const blob = new Blob([data], { type: "text/csv" });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.setAttribute("href", url);
-    a.setAttribute("download", `${canvasID}.csv`);
-    a.click();
-  } catch (error) {
-    console.log(error);
   }
-};
 
-/**
- * @brief
- * Generates the PDF when the button is clicked.
- * @param {String} pdfData - The data of the PDF
- */
-const generatePDF = async (pdfData) => {
-  const blob = new Blob([pdfData], { type: "application/pdf" });
-  const url = URL.createObjectURL(blob);
+  const response = await fetch('/report/renderTemplate', options)
+  const template = await response.text()
 
-  window.open(url);
-};
-
-
-/**
- * @brief
- * Generates the csv when the button is clicked.
- * @param {String} chartId - The data of the csv
- */
-
-const generateCSV = async (chartId) => {
-  console.log('res');
-  const chart = Chart.getChart(chartId);
-  const { datasets, labels } = chart.data;
-  const res = {};
-  res.label = labels;
-  datasets.forEach((dataset) => {
-    res[dataset.label] = dataset.data;
-  });
-
-  return ConvertToCSV(res);
-
+  return template
 }
 
 /**
  * @brief
- * Converts the data to CSV format
- * @param {Object} data - The data to be converted
- * @returns {String} The data in CSV format
+ * Generates a PDF file from the chart
+ * @param {String} canvasID - ID of the chart canvas
+ * @param {String} pageTitle - Title of the page
+ * @returns {void} - Nothing
  */
+const generatePDF = async (canvasID, pageTitle) => {
+  const chartCanvas = document.getElementById(canvasID)
+  const chartImage = chartCanvas.toDataURL('image/png', 1.0)
+  const template = await fetchTemplate({ chartImage, reportTitle: pageTitle })
 
-function ConvertToCSV(data) {
-  const headers = Object.keys(data);
-  const rows = Object.values(data);
+  const pdf = new jsPDF({
+    orientation: 'landscape',
+    unit: 'in',
+    format: [16, 9],
+  })
 
-  let csv = headers.join(",") + "\n";
+  // Zebrands logo
+  pdf.text("Zëbrands",
+    pdf.internal.pageSize.getWidth() - 1.5, 0.5, 1, 1);
+  pdf.text(new Date().toLocaleDateString(), 0.5, 0.5);
 
-  for (let i = 0; i < rows[0].length; i++) {
-    let row = "";
-    for (let j = 0; j < rows.length; j++) {
-      if (j === rows.length - 1) row += rows[j][i];
-      else row += rows[j][i] + ",";
-      if (row.includes("undefined")) {
-        row = row.replace("undefined", "");
-      }
-    }
-    csv += row + "\n";
-  }
-  return csv;
+  // PAGE TITLE (center the text)
+  pdf.setFontSize(20)
+  pdf.text(pageTitle, 8, 0.5, { align: 'center' })
+
+  // Graph
+  pdf.addImage(chartImage, 'PNG', 0.5, 1.5, 15, 7)
+
+  pdf.save(`${pageTitle}.pdf`)
 }
